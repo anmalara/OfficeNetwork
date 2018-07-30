fed034edabff3537ae8c70efadd94afc7427c616
Andrea Malara <andrea.malara@cern.ch>
2018-07-30 11:23:09 +0200

    first Ntuple Creation

---
+++ nTuplesCreation/copy.sh
@@ -0,0 +1,5 @@
+cp /nfs/dust/cms/user/amalara/WorkingArea/UHH2_94/CMSSW_9_4_1/src/UHH2/OfficeNetwork/nTuplesCreation/NTupleCreator.xml /nfs/dust/cms/user/amalara/WorkingArea/UHH2_94/CMSSW_9_4_1/src/UHH2/JER2017/config/NTupleCreator.xml
+cp /nfs/dust/cms/user/amalara/WorkingArea/UHH2_94/CMSSW_9_4_1/src/UHH2/OfficeNetwork/nTuplesCreation/NTupleCreator.cxx /nfs/dust/cms/user/amalara/WorkingArea/UHH2_94/CMSSW_9_4_1/src/UHH2/JER2017/src/NTupleCreator.cxx
+
+cd /nfs/dust/cms/user/amalara/WorkingArea/UHH2_94/CMSSW_9_4_1/src/UHH2/JER2017/
+make -j 20
